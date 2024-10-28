76f44d2179c3785ead5031c345e276cf56ae4598
vigoems <vgomescarvalho59@gmail.com>
2024-10-27 23:14:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,161 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Rússia'
+)
+
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa? ',
+    'O Leonardo da Vinci)'
+)
+
+
+criaCartao(
+    'Ciências',
+    'Qual é o metal mais utilizado no mundo?',
+    'Ferro'
+)
+
+
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais próximo do Sol?',
+    'Mercúrio'
+)
+
+
+criaCartao(
+    'Esportes',
+    'Em qual esporte Michael Jordan se destacou?',
+    'Basquetebol'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    ' Brasília'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento químico mais abundante no universo? ',
+    ' Hidrogênio'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o oceano mais extenso da Terra?',
+    'Oceano Pacífico'
+)
+
+criaCartao(
+    'História',
+    'Qual é o país que inventou o futebol?',
+    ' Inglaterra'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é a montanha mais alta do mundo?',
+    ' Monte Everest'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é a maior floresta tropical do mundo?',
+    'Amazônia.'
+)
+
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu a peça "Romeu e Julieta"?',
+    ' William Shakespeare.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França? ',
+    'Paris'
+)
+
+
+criaCartao(
+    'Cinema',
+    'Qual filme da Disney conta a história de uma princesa que se transforma em uma sereia?',
+    'A Pequena Sereia'
+)
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+
+criaCartao(
+    'Música',
+    'Qual cantor brasileiro é conhecido como o "Rei do Pop"?',
+    'Michael Jackson'
+)
+
+
+criaCartao(
+    'Ciência e Natureza',
+    'Qual é o processo pelo qual as plantas produzem seu próprio alimento?',
+    'Fotossíntese'
+)
+
+
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico da água?',
+    'H₂O'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o continente mais frio do mundo?',
+    ' Antártica'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior deserto do mundo?',
+    'Saara'
+)
